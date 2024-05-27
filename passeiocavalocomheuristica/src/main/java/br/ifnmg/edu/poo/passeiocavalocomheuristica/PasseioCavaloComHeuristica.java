8c2c26d097e5abf8cbb21e740a023ce9eef3d905
André Felipe <140921456+andref03@users.noreply.github.com>
2024-05-27 18:47:24 -0300

    corrigindo bugs e implementando método p/ descobrir melhor movimento

--- passeiocavalocomheuristica/src/main/java/br/ifnmg/edu/poo/passeiocavalocomheuristica/PasseioCavaloComHeuristica.java
+++ passeiocavalocomheuristica/src/main/java/br/ifnmg/edu/poo/passeiocavalocomheuristica/PasseioCavaloComHeuristica.java
@@ -24,17 +24,18 @@ public class PasseioCavaloComHeuristica {
 
         int[] horizontal = {2, 1, -1, -2, -2, -1, 1, 2};
         int[] vertical = {-1, -2, -2, -1, 1, 2, 2, 1};
-        int[] acessibilidade = {1, 2, 3, 4, 5, 6, 7, 8};
 
         boolean quadradoValido = true; // booleano p/ impedir laço while infinito
 
-        int contador = 0;
+        int movimento = 0;
 
         tabuleiro[linhaAtual][colunaAtual] = 0;
 
-        while (contador <= 64 && quadradoValido) {
+        while (movimento <= 64 && quadradoValido) {
             int proxLinha;
             int proxColuna;
+            int melhorMovimento = descobreMelhorMovimento(tabuleiro, linhaAtual, colunaAtual);
+
             quadradoValido = false;
 
             for (int moveNumber = 0; moveNumber < 8; moveNumber++) {
@@ -43,24 +44,43 @@ public class PasseioCavaloComHeuristica {
 
                 if (proxLinha >= 0 && proxLinha < 8 && proxColuna >= 0 && proxColuna < 8 && tabuleiro[proxLinha][proxColuna] != 0) {
 
-                    // passa por todos os pesos, priorizando os menores (+ inacessíveis)
-                    for (int acesso = 0; acesso < 8; acesso++) {
-                        if (acessibilidade[acesso] == tabuleiro[proxLinha][proxColuna]) {
-                            linhaAtual = proxLinha;
-                            colunaAtual = proxColuna;
-                            contador++;
-                            tabuleiro[linhaAtual][colunaAtual] = 0;
-                            quadradoValido = true;
-                            atualizaAcessibilidade(linhaAtual, colunaAtual, tabuleiro);
-                            break; // volta pro while(), p/ avançar a próx. posição
-                        }
+                    if (tabuleiro[proxLinha][proxColuna] == melhorMovimento) {
+                        linhaAtual = proxLinha;
+                        colunaAtual = proxColuna;
+                        movimento++;
+                        tabuleiro[linhaAtual][colunaAtual] = 0;
+                        quadradoValido = true;
+                        atualizaAcessibilidade(linhaAtual, colunaAtual, tabuleiro);
+                        break; // volta pro while(), p/ avançar a próx. posição
                     }
 
                 }
             }
         }
 
-        return contador;
+        return movimento;
+    }
+
+    private static int descobreMelhorMovimento(int[][] tabuleiro, int linhaAtual, int colunaAtual) {
+
+        int[] horizontal = {2, 1, -1, -2, -2, -1, 1, 2};
+        int[] vertical = {-1, -2, -2, -1, 1, 2, 2, 1};
+        int proxLinha;
+        int proxColuna;
+        int menorNumero = 9; // pois 9 com certeza será maior que qlqr nº de acessibilidade desse tabuleiro
+
+        for (int moveNumber = 0; moveNumber < 8; moveNumber++) {
+            proxLinha = linhaAtual + vertical[moveNumber];
+            proxColuna = colunaAtual + horizontal[moveNumber];
+
+            if (proxLinha >= 0 && proxLinha < 8 && proxColuna >= 0 && proxColuna < 8 && tabuleiro[proxLinha][proxColuna] != 0) {
+                if (tabuleiro[proxLinha][proxColuna] < menorNumero) {
+                    menorNumero = tabuleiro[proxLinha][proxColuna]; // atualiza com o menor nº
+                }
+            }
+        }
+
+        return menorNumero;
     }
 
     private static void atualizaAcessibilidade(int linhaAtual, int colunaAtual, int[][] tabuleiro) {
